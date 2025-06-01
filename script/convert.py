4900d46fe07479b5089408a0d5ab9beba0f8ed28
whichxjy <whichxjy@gmail.com>
2025-06-01 21:59:17 +0800

    Update

--- script/convert.py
+++ script/convert.py
@@ -143,7 +143,7 @@ input = """
 ~	~	~	~	~	~	4F-8	~	5Eb-8	~	5Db-8	~	5C-8	~	4Ab-8	~
 
 2Ab-8	~	3F-8	~	3Ab-8	~	4C-8	~	3C-8	~	3Ab-8	~	4C-8	~	4Eb-8	~
-3Db-8	~	3Ab-8		4Db-8	~	4Eb-8		2Bb-8	~	3F-8	~	4Db-8	~	4C-8	~
+3Db-8	~	3Ab-8	~	4Db-8	~	4Eb-8	～	2Bb-8	~	3F-8	~	4Db-8	~	4C-8	~
 """
 
 print(convert_music_notation(input))
