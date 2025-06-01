bd0e280840c9da35f3bd5509fba9b8085f15f2a9
whichxjy <whichxjy@gmail.com>
2025-06-02 03:22:51 +0800

    Update

--- project/input.py
+++ project/input.py
@@ -1,3 +1,8 @@
+pattern0 = """
+6C-5	~	~	~	~	~	~	~	6C-5	~	~	~	~	~	~	~
+6C-5	~	~	~	~	~	~	~	6C-5	~	~	~	~	~	~	~
+"""
+
 pattern1 = """
 5C-5	~	~	~	4F-5	~	~	~	4Ab-5	~	~	~	~	~	4Ab-5	4F-5
 ~	~	~	~	~	~	4F-5	~	5Eb-5	~	5Db-5	~	5C-5	~	4Ab-5	~
@@ -25,5 +30,5 @@ pattern3 = """
 2Ab-5	~	3Ab-5	~	3F-5	~	3Eb-5	~	2Db-5	~	3Db-5	~	3Eb-5	~	3Db-5	~
 """
 
-__patterns__: list[str] = [pattern1, pattern2, pattern3]
+__patterns__: list[str] = [pattern0, pattern1, pattern2, pattern3]
 __output_path__ = "project/patterns"
--- script/gen.py
+++ script/gen.py
@@ -5,7 +5,7 @@ from project.input import __patterns__, __output_path__
 
 patterns_dir = Path(__output_path__)
 patterns_dir.mkdir(parents=True, exist_ok=True)
-for i, pattern in enumerate(__patterns__, start=1):
+for i, pattern in enumerate(__patterns__):
     result = convert_music_notation(pattern) + "\n"
 
     file_path = patterns_dir / f"pattern{i}.txt"
