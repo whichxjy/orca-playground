f3a4cf22f19d48dc5c6b9766fcc3e34d3bf363ae
whichxjy <whichxjy@gmail.com>
2025-07-24 01:41:13 +0800

    Update

--- project/pocket_calculator/input.py
+++ project/pocket_calculator/input.py
@@ -1,8 +1,13 @@
 pattern0 = """
 5C-A	4D#-A	5F#-A	3F-A	5C-A	3G#-A	x	x	x	x	x	x	x	x	x	x
-x	x	x	x	x	x	x	x	x	x	x	x	x	x	x	x
+5C-A	4D#-A	5F#-A	3F-A	5C-A	3G#-A	x	x	x	x	x	x	x	x	x	x
+"""
+
+pattern1 = """
+6C-A	5D#-A	6F#-A	4F-A	6C-A	4G#-A	x	x	x	x	x	x	x	x	x	x
+6C-A	5D#-A	6F#-A	4F-A	6C-A	4G#-A	x	x	x	x	x	x	x	x	x	x
 """
 
 
-__patterns__: list[str] = [pattern0]
+__patterns__: list[str] = [pattern0, pattern1]
 __output_path__ = "project/pocket_calculator/patterns"
